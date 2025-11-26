ca5d0f28467917498a3382dc8a3cc03e1812a99a
mizzi-sesh <dulcinea.mizzi@gmail.com>
2025-11-26 17:12:36 +0000

    added logging and message history

--- BlazorSignalRApp/BlazorSignalRApp/Hubs/ChatHub.cs
+++ BlazorSignalRApp/BlazorSignalRApp/Hubs/ChatHub.cs
@@ -1,11 +1,65 @@
+using System.Diagnostics;
+using BlazorSignalRApp.Components;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorSignalRApp.Hubs;
 
+
+[ApiController]
+[Route("api/[controller]")]
+public class MessagesController : ControllerBase
+{
+    private readonly AppDBContext _dbContext;
+
+    public MessagesController(AppDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IEnumerable<Message> GetMessages()
+    {
+        // Does it have to be a list?
+        return _dbContext.Messages.Include(m => m.SenderId).ToList();
+    }
+}
+
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+
+    private readonly AppDBContext _dbContext;
+    private readonly ILogger _logger;
+
+		public ChatHub(AppDBContext dbContext, ILogger<ChatHub>logger)
+		{
+			_dbContext = dbContext;
+            _logger = logger; 
+		}
+		
+    public async Task SendMessage(string user, string messageContent)
+	{
+
+			var message = new Message(content: messageContent, sentTime: DateTime.Now, senderId: user);
+
+			_dbContext.Messages.Add(message);
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            
+			await _dbContext.SaveChangesAsync();
+			
+    }
+
+    public async Task<List<string>> InitialiseMessageHistory(string connectionId)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+        var serialisedListOfMessages = await _dbContext.Messages
+            .Select(m => m.ToString())
+            .ToListAsync();
+       
+        await Clients.All.SendAsync("LoadMessages", connectionId, serialisedListOfMessages);
+
+        return serialisedListOfMessages;
     }
 }
--- BlazorSignalRApp/BlazorSignalRApp/Program.cs
+++ BlazorSignalRApp/BlazorSignalRApp/Program.cs
@@ -2,8 +2,21 @@ using BlazorSignalRApp.Client.Pages;
 using BlazorSignalRApp.Components;
 using Microsoft.AspNetCore.ResponseCompression;
 using BlazorSignalRApp.Hubs;
+using BlazorSignalRApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("AppDbContextSQLite"));
+});
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter(); 
 
 // Add services to the container.
 builder.Services.AddSignalR();
@@ -30,9 +43,20 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AppDBContext>();
+    context.Database.EnsureCreated();
+    
 }
 
 app.UseHttpsRedirection();
